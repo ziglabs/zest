fca4d1a21df10db5168c4c4bacd7c1f3018b5cad
Jon-Eric Cook <joneric.cook@gmail.com>
2023-01-04 09:13:18 -0800

    save work

--- src/method.zig
+++ src/method.zig
@@ -10,38 +10,37 @@ pub const MethodError = error{
 pub const Method = enum {
     post,
 
-    // https://www.rfc-editor.org/rfc/rfc9110.html#name-methods
-    pub const methods = [_][]const u8{ "POST" };
-
     pub fn toString(self: Method) []const u8 {
         return methods[@enumToInt(self)];
     }
-
-    pub fn fromString(method: []const u8) MethodError!Method {
-        for (methods) |m, i| {
-            if (std.mem.eql(u8, m, method)) {
-                return @intToEnum(Method, i);
-            }
-        }
-        return MethodError.UnsupportedMethod;
-    }
 };
+
+// https://www.rfc-editor.org/rfc/rfc9110.html#name-methods
+pub const methods = [_][]const u8{"POST"};
+
+pub fn fromString(method: []const u8) MethodError!Method {
+    for (methods) |m, i| {
+        if (std.mem.eql(u8, m, method)) {
+            return @intToEnum(Method, i);
+        }
+    }
+    return MethodError.UnsupportedMethod;
+}
 
 test "lengths are equal" {
     const methods_enum_length = @typeInfo(Method).Enum.fields.len;
-    try expect(methods_enum_length == Method.methods.len);
+    try expect(methods_enum_length == methods.len);
 }
 
 test "invalid values return an error" {
     const expected_error = MethodError.UnsupportedMethod;
-    try expectError(expected_error, Method.fromString(""));
-    try expectError(expected_error, Method.fromString(" "));
-    try expectError(expected_error, Method.fromString("HELLO"));
+    try expectError(expected_error, fromString(""));
+    try expectError(expected_error, fromString(" "));
+    try expectError(expected_error, fromString("HELLO"));
 }
 
 test "method POST" {
     const method = Method.post;
     try expect(std.mem.eql(u8, method.toString(), "POST"));
-    try expect(try Method.fromString("POST") == Method.post);
+    try expect(try fromString("POST") == Method.post);
 }
-
--- src/scheme.zig
+++ src/scheme.zig
@@ -11,111 +11,43 @@ pub const Scheme = enum {
     http,
     https,
 
-    // https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
-    pub const schemes = [_][]const u8{ "http", "https" };
-
     pub fn toString(self: Scheme) []const u8 {
         return schemes[@enumToInt(self)];
     }
-
-    pub fn fromString(scheme: []const u8) SchemeError!Scheme {
-        for (schemes) |v, i| {
-            if (std.mem.eql(u8, v, scheme)) {
-                return @intToEnum(Scheme, i);
-            }
-        }
-        return SchemeError.UnsupportedScheme;
-    }
-
-    pub fn parse(input: []const u8) SchemeError!Scheme {
-        if (std.mem.startsWith(u8, input, Scheme.https.toString())) {
-            return Scheme.https;
-        } else if (std.mem.startsWith(u8, input, Scheme.http.toString())) {
-            return Scheme.http;
-        }
-        return SchemeError.UnsupportedScheme;
-    }
-
-    pub fn length(self: Scheme) u8 {
-        return @intCast(u8, self.toString().len);
-    }
-
-    pub fn removeScheme(input: []const u8, scheme: Scheme) SchemeError![]const u8 {
-        if (std.mem.startsWith(u8, input, scheme.toString())) {
-            return input[scheme.length()..];
-        } else {
-            return SchemeError.UnsupportedScheme;
-        }
-    }
 };
+
+// https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
+pub const schemes = [_][]const u8{ "http", "https" };
+
+pub fn fromString(scheme: []const u8) SchemeError!Scheme {
+    for (schemes) |v, i| {
+        if (std.mem.eql(u8, v, scheme)) {
+            return @intToEnum(Scheme, i);
+        }
+    }
+    return SchemeError.UnsupportedScheme;
+}
 
 test "lengths are equal" {
     const schemes_enum_length = @typeInfo(Scheme).Enum.fields.len;
-    try expect(schemes_enum_length == Scheme.schemes.len);
+    try expect(schemes_enum_length == schemes.len);
 }
 
 test "invalid values return an error" {
     const expected_error = SchemeError.UnsupportedScheme;
-    try expectError(expected_error, Scheme.fromString(""));
-    try expectError(expected_error, Scheme.fromString(" "));
-    try expectError(expected_error, Scheme.fromString("HELLO"));
+    try expectError(expected_error, fromString(""));
+    try expectError(expected_error, fromString(" "));
+    try expectError(expected_error, fromString("HELLO"));
 }
 
 test "scheme http" {
     const scheme = Scheme.http;
     try expect(std.mem.eql(u8, scheme.toString(), "http"));
-    try expect(try Scheme.fromString("http") == Scheme.http);
+    try expect(try fromString("http") == Scheme.http);
 }
 
 test "scheme https" {
     const scheme = Scheme.https;
     try expect(std.mem.eql(u8, scheme.toString(), "https"));
-    try expect(try Scheme.fromString("https") == Scheme.https);
-}
-
-test "scheme parse http" {
-    const scheme = try Scheme.parse("http://");
-    try expect(scheme == Scheme.http);
-}
-
-test "scheme parse https" {
-    const scheme = try Scheme.parse("https://");
-    try expect(scheme == Scheme.https);
-}
-
-test "scheme parse hello" {
-    const expected_error = SchemeError.UnsupportedScheme;
-    try expectError(expected_error, Scheme.parse("hello://"));
-}
-
-test "scheme parse blank" {
-    const expected_error = SchemeError.UnsupportedScheme;
-    try expectError(expected_error, Scheme.parse(""));
-}
-
-test "scheme len http" {
-    const scheme = Scheme.http;
-    try expect(scheme.length() == 4);
-}
-
-test "scheme len https" {
-    const scheme = Scheme.https;
-    try expect(scheme.length() == 5);
-}
-
-test "scheme removeScheme" {
-    const url = "https://hello.com";
-    const scheme = Scheme.https;
-    const result = try Scheme.removeScheme(url, scheme);
-    try expect(std.mem.eql(u8, result, "://hello.com"));
-}
-
-test "scheme removeScheme empty input" {
-    const expected_error = SchemeError.UnsupportedScheme;
-    try expectError(expected_error, Scheme.removeScheme("", Scheme.https));
-}
-
-test "scheme removeScheme no match" {
-    const expected_error = SchemeError.UnsupportedScheme;
-    try expectError(expected_error, Scheme.removeScheme("hello", Scheme.https));
+    try expect(try fromString("https") == Scheme.https);
 }
