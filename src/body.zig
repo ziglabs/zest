620ed1a643102840580e04faaf68d3a2bcecef7a
Jon-Eric Cook <joneric.cook@gmail.com>
2023-01-18 20:43:17 -0800

    save work

--- src/body.zig
+++ src/body.zig
@@ -1,63 +1,91 @@
 const std = @import("std");
 const json = std.json;
 const expect = std.testing.expect;
+const expectError = std.testing.expectError;
+const expectEqualStrings = std.testing.expectEqualStrings;
 
+pub const BodyError = error {
+    CannotParseBody,
+    CannotStringifyBody,
+};
 
-pub fn parse(comptime T: type, body: []const u8) !T {
-    var stream = json.TokenStream.init(body);
-    return try json.parse(T, &stream, .{});
-}
-
-pub fn stringify(buffer: []u8, comptime T: type, content: T) ![]const u8 {
+pub fn parse(buffer: []u8, comptime T: type, body: []const u8) !T {
     var fba = std.heap.FixedBufferAllocator.init(buffer);
-    return try std.json.stringifyAlloc(fba.allocator(), content, .{});
+    var stream = json.TokenStream.init(body);
+    return try json.parse(T, &stream, .{.allocator = fba.allocator()});
 }
 
-test "testing 1" {
-    const Config = struct {
-        vals: struct { testing: u8, production: u8 },
-        uptime: u64,
-    };
-
-    const body =
-        \\{
-        \\    "vals": {
-        \\        "testing": 1,
-        \\        "production": 42
-        \\    },
-        \\    "uptime": 9999
-        \\}
-    ;
-    const result = try parse(Config, body);
-    try expect(result.uptime == 9999);
+pub fn stringify(buffer: []u8, comptime T: type, content: T) BodyError![]const u8 {
+    var fba = std.heap.FixedBufferAllocator.init(buffer);
+    return std.json.stringifyAlloc(fba.allocator(), content, .{}) catch BodyError.CannotStringifyBody;
 }
 
-test "testing 2" {
+// test "testing 1" {
+//     const Config = struct {
+//         vals: struct { testing: u8, production: u8 },
+//         uptime: u64,
+//     };
+
+//     const body =
+//         \\{
+//         \\    "vals": {
+//         \\        "testing": 1,
+//         \\        "production": 42
+//         \\    },
+//         \\    "uptime": 9999
+//         \\}
+//     ;
+//     const result = try parse(Config, body);
+//     try expect(result.uptime == 9999);
+// }
+
+// test "testing 2" {
+//     const Config = struct {
+//         uptime: u64
+//     };
+
+//     const body = "{\"uptime\": 9999}";
+//     const result = try parse(Config, body);
+//     try expect(result.uptime == 9999);
+// }
+
+// test "testing 3" {
+//     const Config = struct {
+//         greeting: [4]u8
+//     };
+
+//     const body = "{\"greeting\": \"9999\"}";
+//     const result = try parse(Config, body);
+//     try expectEqualStrings(&result.greeting, "9999");
+// }
+
+// test "testing 4" {
+//     const Config = struct {
+//         uptime: u64
+//     };
+//     const config = Config{ .uptime = 4};
+//     var buffer: [1024]u8 = undefined;
+//     const result = try stringify(&buffer, Config, config);
+//     try expectEqualStrings("{\"uptime\":4}", result);
+// }
+
+// test "testing 5" {
+//     const Config = struct {
+//         greeting: [2]u8
+//     };
+
+//     const body = "{\"greeting\": \"9999\"}";
+//     try expectError(BodyError.CannotParseBody, parse(Config, body));
+// }
+
+test "testing 6" {
     const Config = struct {
-        uptime: u64
+        greeting: []const u8,
+        hello: []const u8,
     };
-
-    const body = "{\"uptime\": 9999}";
-    const result = try parse(Config, body);
-    try expect(result.uptime == 9999);
-}
-
-test "testing 3" {
-    const Config = struct {
-        greating: [4]u8
-    };
-
-    const body = "{\"greating\": \"9999\"}";
-    const result = try parse(Config, body);
-    try expect(std.mem.eql(u8, &result.greating, "9999"));
-}
-
-test "testing 4" {
-    const Config = struct {
-        uptime: u64
-    };
-    const config = Config{ .uptime = 4};
-    var buffer: [1024]u8 = undefined;
-    const result = try stringify(&buffer, Config, config);
-    try expect(std.mem.eql(u8, "{\"uptime\":4}", result));
+    var buffer: [6]u8 = undefined;
+    const body = "{\"greeting\": \"9999\", \"hello\": \"88\"}";
+    const result = try parse(&buffer, Config, body);
+    try expectEqualStrings(result.greeting, "9999");
+    try expectEqualStrings(result.hello, "88");
 }
