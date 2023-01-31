fec67875c14fa5455e4568bc30099a130e35f3cb
Jon-Eric Cook <joneric.cook@gmail.com>
2023-01-30 22:33:17 -0800

    save work

--- src/route.zig
+++ src/route.zig
@@ -1,10 +1,18 @@
-const method = @import("method.zig");
 const message = @import("message.zig");
 
-pub const Route = struct {
-    method: method.Method,
-    path: []const u8,
-    handler: Handler,
+const Handler = fn (message.Message, message.Message) anyerror!void;
 
-    const Handler = fn (message.Message, message.Message) anyerror!void;
-};
+pub fn Route(comptime RequestBody: type, comptime ResponseBody: type) type {
+    if (@typeInfo(RequestBody) != .Struct) @compileError("Route expects a struct type for RequestBody");
+    if (@typeInfo(ResponseBody) != .Struct) @compileError("Route expects a struct type for ResponseBody");
+    return struct {
+        path: []const u8,
+        request_body: RequestBody,
+        response_body: ResponseBody,
+        handler: Handler,
+    };
+}
+
+pub fn Build(path: []const u8, comptime RequestBody: type, request_body: RequestBody, comptime ResponseBody: type, response_body: ResponseBody, handler: Handler) Route(RequestBody, ResponseBody) {
+    return Route(RequestBody, ResponseBody){ .path = path, .request_body = request_body, .response_body = response_body, .handler = handler };
+}
---
+++ src/router.zig
@@ -0,0 +1,7 @@
+const route = @import("route.zig");
+
+pub const Router = struct {
+    routes: []const route.Route,
+
+
+};
