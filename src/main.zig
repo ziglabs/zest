bde5a5b91ae1a7c1dac3f03a110557c5e83e3d30
Jon-Eric Cook <joneric.cook@gmail.com>
2023-02-05 19:19:23 -0800

    save work

--- src/main.zig
+++ src/main.zig
@@ -9,14 +9,22 @@ const No = struct {
     bye: u8,
 };
 
-fn yoyo(req: zest.request.Request(Yes), res: *zest.response.Response(No)) anyerror!void {
+fn hello(req: zest.request.Request(Yes), res: *zest.response.Response(No)) anyerror!void {
     _ = req;
     try res.headers.put("Dog", "8");
     res.body = No{ .bye = 10 };
 }
 
+fn hi(req: zest.request.Request(zest.request.EmptyBody), res: *zest.response.Response(zest.response.EmptyBody)) anyerror!void {
+    _ = req;
+    try res.headers.put("Dog", "8");
+}
+
 pub fn main() !void {
     const config = comptime try zest.server.Config.default();
-    const routes = .{try zest.route.Build("/hello", Yes, No, yoyo)};
+    const routes = comptime .{ 
+        try zest.route.Build("/hello", Yes, No, hello), 
+        try zest.route.Build("/hi", zest.request.EmptyBody, zest.response.EmptyBody, hi) 
+    };
     try zest.server.start(config, routes);
 }
--- src/server.zig
+++ src/server.zig
@@ -8,11 +8,11 @@ const res = @import("response.zig");
 const sl = @import("status_line.zig");
 const h = @import("headers.zig");
 const b = @import("body.zig");
-const status = @import("status.zig");
-const version = @import("version.zig");
+const s = @import("status.zig");
+const v = @import("version.zig");
 
-// address default to 127.0.0.1:8080
-// all byte counts default to 1kb
+// address defaults to 127.0.0.1:8080
+// all byte counts defaults to 1kb
 pub const Config = struct {
     address: std.net.Address,
     max_read_request_line_bytes: u64 = 1024,
@@ -33,7 +33,8 @@ pub const Config = struct {
     }
 };
 
-pub fn start(comptime config: Config, routes: anytype) !void {
+pub fn start(comptime config: Config, comptime routes: anytype) !void {
+    if (@typeInfo(@TypeOf(routes)) != .Struct) @compileError("start expects routes to be an anonymous list literal");
     var server = net.StreamServer.init(.{ .reuse_address = true });
     defer server.deinit();
     try server.listen(config.address);
@@ -89,16 +90,40 @@ pub fn start(comptime config: Config, routes: anytype) !void {
         const read_request_line = try r.readUntilDelimiterAlloc(read_request_line_fba.allocator(), '\r', config.max_read_request_line_bytes);
         const request_line = try rl.parse(read_request_line);
 
-        const route = get_route: inline for (routes) |route| {
-            if (std.mem.eql(u8, route.path, request_line.path)) {
-                break :get_route route;
-            }
-        } else {
-            try w.writeAll("HTTP/1.1 404\r\n\r\n");
-            try bw.flush();
-            connection.stream.close();
-            continue;
-        };
+        // const route = get_route: inline for (std.meta.fields(@TypeOf(routes))) |field| {
+        //     const route = @field(routes, field.name);
+        //     if (std.mem.eql(u8, route.path, request_line.path)) {
+        //         break :get_route route;
+        //     }
+        // } else {
+        //     try w.writeAll("HTTP/1.1 404\r\n\r\n");
+        //     try bw.flush();
+        //     connection.stream.close();
+        //     continue;
+        // };
+
+        // const route = get_route: inline for (routes) |route| {
+        //     if (std.mem.eql(u8, route.path, request_line.path)) {
+        //         break :get_route route;
+        //     }
+        // } else {
+        //     try w.writeAll("HTTP/1.1 404\r\n\r\n");
+        //     try bw.flush();
+        //     connection.stream.close();
+        //     continue;
+        // };
+
+        // const route = get_route: inline for (@typeInfo(@TypeOf(routes)).Struct.fields) |field| {
+        //     const route = @field(routes, field.name);
+        //     if (std.mem.eql(u8, route.path, request_line.path)) {
+        //         break :get_route route;
+        //     }
+        // } else {
+        //     try w.writeAll("HTTP/1.1 404\r\n\r\n");
+        //     try bw.flush();
+        //     connection.stream.close();
+        //     continue;
+        // };
 
         // skips the \n
         try r.skipBytes(1, .{});
@@ -148,12 +173,18 @@ pub fn start(comptime config: Config, routes: anytype) !void {
         const request_body = try b.parse(request_body_parse_fba.allocator(), route.request_body_type, request_body_buffer);
         const request = req.Build(request_line, request_headers_map, route.request_body_type, request_body);
         var response_headers_map = h.Headers.init(response_headers_map_fba.allocator());
-        var response = res.Build(sl.StatusLine{ .version = version.Version.http11, .status = status.Status.ok }, response_headers_map, route.response_body_type, response_body_fba.allocator());
+        var response = res.Build(sl.StatusLine{ .version = v.Version.http11, .status = s.Status.ok }, response_headers_map, route.response_body_type, response_body_fba.allocator());
         try route.handler(request, &response);
 
         std.debug.print("\nDog: {s}", .{response.headers.get("Dog") orelse unreachable});
-        const response_body = response.body orelse unreachable;
-        std.debug.print("\nbye: {d}", .{response_body.bye});
+        const response_body = if (response.body) |body| body else {
+            try w.writeAll("HTTP/1.1 500\r\n\r\n");
+            try bw.flush();
+            connection.stream.close();
+            continue;
+        };
+        _ = response_body;
+        // std.debug.print("\nbye: {d}", .{response_body.bye});
         try w.writeAll("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/plain\r\nContent-Length: 2\r\n\r\nhi");
         try bw.flush();
         connection.stream.close();
