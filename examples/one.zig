f91a69329504aabaff7d4c7f25f03de38c6f4341
Jon-Eric Cook <joneric.cook@gmail.com>
2023-02-09 19:59:56 -0800

    add checks for valid json i request and response

--- examples/one.zig
+++ examples/one.zig
@@ -18,13 +18,14 @@ const Friend = struct {
 
 fn findFriend(req: Request, res: *Response) anyerror!void {
     const request_body = try req.parseBody(Person);
-    const response_body = Friend{ .name = "Zippy McZappy", .age = request_body.age * 2 };
+    const friend_name = try std.mem.join(res.body_allocator, " ", &.{ "Zippy", request_body.name, "McZappy" });
+    const response_body = Friend{ .name = friend_name, .age = request_body.age * 2 };
     try res.stringifyBody(Friend, response_body);
 }
 
 pub fn main() !void {
     const config = comptime try server.Config.init("127.0.0.1", 8080, 1024);
-    const routes = comptime .{ try Route.init("/findFriend", findFriend) };
+    const routes = comptime .{try Route.init("/findFriend", findFriend)};
     const router = comptime Router.init(&routes);
     try server.start(config, router);
 }
--- src/server.zig
+++ src/server.zig
@@ -108,14 +108,12 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
                 continue;
             },
         };
-
         const route = router.find(request_line.path) orelse {
             try w.writeAll("HTTP/1.1 404\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         };
-
         // skips the \n
         r.skipBytes(1, .{}) catch {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
@@ -123,7 +121,6 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
             connection.stream.close();
             continue;
         };
-
         var request_headers_map = h.Headers.init(request_headers_map_fba.allocator());
         read_request_headers: while (true) {
             const read_request_header = r.readUntilDelimiterAlloc(read_request_headers_fba.allocator(), '\r', config.max_read_request_headers_bytes) catch {
@@ -147,7 +144,6 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
                 continue;
             };
         }
-
         // skips the \n
         r.skipBytes(1, .{}) catch {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
@@ -155,42 +151,36 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
             connection.stream.close();
             continue;
         };
-
         const content_length_string = request_headers_map.get("Content-Length") orelse {
             try w.writeAll("HTTP/1.1 411\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         };
-
         const content_length_number = std.fmt.parseUnsigned(u64, content_length_string, 10) catch {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         };
-
         if (content_length_number > config.max_read_request_body_bytes) {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         }
-
         const content_type = request_headers_map.get("Content-Type") orelse {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         };
-
         if (!std.mem.eql(u8, content_type, "application/json")) {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         }
-
         var request_body_raw = read_request_body_fba.allocator().alloc(u8, content_length_number) catch {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
             try bw.flush();
@@ -203,14 +193,18 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
             connection.stream.close();
             continue;
         };
-
+        if (!std.json.validate(request_body_raw)) {
+            try w.writeAll("HTTP/1.1 400\r\n\r\n");
+            try bw.flush();
+            connection.stream.close();
+            continue;
+        }
         if (read_request_body_count > content_length_number) {
             try w.writeAll("HTTP/1.1 400\r\n\r\n");
             try bw.flush();
             connection.stream.close();
             continue;
         }
-
         const request = req.Request{ .request_line = request_line, .headers = request_headers_map, .body_raw = request_body_raw, .body_allocator = request_body_parse_fba.allocator() };
         var response_headers_map = h.Headers.init(response_headers_map_fba.allocator());
         var response = res.Response{
@@ -240,10 +234,14 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
                 continue;
             },
         };
-
+        if (!std.json.validate(response.body_raw)) {
+            try w.writeAll("HTTP/1.1 500\r\n\r\n");
+            try bw.flush();
+            connection.stream.close();
+            continue;
+        }
         // status line
         try w.print("{s} {s}\r\n", .{ response.status_line.version.toString(), response.status_line.status.toString() });
-
         // headers
         try w.writeAll("Connection: close\r\n");
         try w.writeAll("Content-Type: application/json\r\n");
@@ -255,7 +253,6 @@ pub fn start(comptime config: Config, comptime router: Router) !void {
         }
         // body
         try w.print("\r\n{s}", .{response.body_raw});
-
         try bw.flush();
         connection.stream.close();
     }
